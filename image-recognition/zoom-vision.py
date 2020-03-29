72f8e8ad1bfcafed1dc09e848d7b6e76f0f20028
Shivaen Ramshetty <44125228+sramshetty@users.noreply.github.com>
2020-03-29 16:53:23 -0400

    working video and vision components

--- image-recognition/zoom-vision.py
+++ image-recognition/zoom-vision.py
@@ -2,20 +2,33 @@ import os, io
 from google.cloud import vision
 import pandas as pd
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS']=r'google-suite-token.json'
-client = vision.ImageAnnotatorClient()
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']=r'zoom-token_2.json'
 
-image = vision.types.image('gs://zoom-education-suite-videos/happy.jpg')
-reponse = client.label_detection(image=image) 
-labels = reponse.label_annotations
+def emotion_recognition(file_path):
+    client = vision.ImageAnnotatorClient()
 
-df = pd.DataFrame(columns=['description', 'score', 'topicality'])
-for i in labels:
-    df = df.append(
-        dict(
-            description=i.description,
-            score=i.score,
-            topicality=i.topicality
-        ), ignore_index=True
-    )
-print(df)
+    image_path = file_path
+
+    with io.open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+    response = client.face_detection(image=image)
+    faceAnnotations = response.face_annotations
+
+    likehood = ('Unknown', 'Very Unlikely', 'Unlikely', 'Possibly', 'Likely', 'Very Likely')
+
+    print('Faces:')
+    for face in faceAnnotations:
+        print('Detection Confidence {0}'.format(face.detection_confidence))
+        print('Angry likelyhood: {0}'.format(likehood[face.anger_likelihood]))
+        print('Joy likelyhood: {0}'.format(likehood[face.joy_likelihood]))
+        print('Sorrow likelyhood: {0}'.format(likehood[face.sorrow_likelihood]))
+        print('Surprised ikelihood: {0}'.format(likehood[face.surprise_likelihood]))
+        print('Headwear likelyhood: {0}'.format(likehood[face.headwear_likelihood]))
+
+        face_vertices = ['({0},{1})'.format(vertex.x, vertex.y) for vertex in face.bounding_poly.vertices]
+        print('Face bound: {0}'.format(', '.join(face_vertices)))
+        print('')
+
+print(emotion_recognition("happy.jpg"))
